618ce9d721fc9ffb491e0e8ceec1f44bd47f5f57
shoretick <shoretick1@gmail.com>
2022-11-22 11:54:44 -0300

    models and relations

--- PI-Pokemon-main/api/src/models/Pokemon.js
+++ PI-Pokemon-main/api/src/models/Pokemon.js
@@ -4,9 +4,49 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('pokemon', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
+    createdInDB:{ 
+      type: DataTypes.BOOLEAN,
+      default: false, 
+      defaultValue: true
+
+  },
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    allowNull: false,
+    primaryKey: true,
+  },
+  
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+
+  life: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+
+  attack: {
+    type: DataTypes.FLOAT
+  },
+
+  defending:{
+    type: DataTypes.FLOAT
+  },
+
+  image: {
+    type: DataTypes.STRING
+  },
+
+  speed: {
+    type: DataTypes.STRING
+  },
+  height: {
+    type: DataTypes.FLOAT
+  },
+  weight: {
+    type: DataTypes.STRING
+  }
   });
 };
